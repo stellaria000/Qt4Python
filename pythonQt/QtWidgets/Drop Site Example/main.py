3a1f0f0ba0613418b7d14cc9d2d26bcb85c38390
syyou <syyou@pintel.co.kr>
2024-06-28 15:47:37 +0900

    Qt for Python more examples completed  or other file codes revised

--- pythonQt/QtWidgets/Drop Site Example/main.py
+++ pythonQt/QtWidgets/Drop Site Example/main.py
@@ -3,4 +3,13 @@
     AND DROP OPERATION.
     IT ACCEPTS DROPS FROM OTHER APPLICATIONS AND DISPLAYS THE MIME FORMATS PROVIDED BY THE DRAG OBJECT.
 """
+import sys
 
+from PySide6.QtWidgets import QApplication
+from dropsitewindow import DropSiteWindow
+
+if __name__== "main":
+    app= QApplication(sys.argv)
+    window= DropSiteWindow()
+    window.show()
+    sys.exit(app.exec())
